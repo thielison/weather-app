087b929d7ec25552ac55a44cdcdfc8aed4ff1a11
Thielison <67246254+thielison@users.noreply.github.com>
2024-06-17 22:13:41 -0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    performance: {
+        hints: false,
+    },
+    mode: "production",
+    entry: "./src/index.js",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+        }),
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { targets: "defaults" }]],
+                    },
+                },
+            },
+        ],
+    },
+};
