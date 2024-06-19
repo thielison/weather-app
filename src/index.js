a382ee7505642e568807bad330d5ab54d82f0d2f
Thielison <67246254+thielison@users.noreply.github.com>
2024-06-18 21:44:02 -0300

    Add initial functions to fetch and display data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import "./styles/main.css";
+
+const apiKey = "fd654da885e9428aaa611606240606";
+const para = document.getElementById("weather-info-para");
+
+const fetchWeatherData = async (location) => {
+    try {
+        const response = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
+            { mode: "cors" }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Fetch error: ", error);
+        throw error;
+    }
+};
+
+const showWeatherData = async (location) => {
+    try {
+        const data = await fetchWeatherData(location);
+        para.textContent = data.current.condition.text;
+    } catch (error) {
+        console.error("Error displaying weather data: ", error);
+    }
+};
+
+document.getElementById("submit-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    const location = document.getElementById("location").value;
+    showWeatherData(location);
+});
